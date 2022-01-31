2bbcf69fc2b49ff54a40de32abf319b8d3aa1547
micserv010821 <dotnet290320@gmail.com>
2022-01-31 18:29:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express')
 const router = express.Router();
 const path = require('path')
@@ -11,7 +10,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/')))
 // localhost:8080/page1.html
 
-app.get('/page', (req, resp) => 
+app.get('/', (req, resp) => 
 {
     resp.writeHead(200);
     resp.end('this page will be sent on default url')
@@ -19,7 +18,7 @@ app.get('/page', (req, resp) =>
 app.get('/fruit', (req, resp) => 
 {
     resp.writeHead(201);
-    resp.end('{ id: 1, "name": "banana"}')
+    resp.end('banana is my favorite fruit')
 })
 
 app.get('/p1', (req, resp) => 
@@ -27,6 +26,30 @@ app.get('/p1', (req, resp) =>
     resp.sendFile(path.join(__dirname + '/page1.html'));
 });
 
-// map /random ==> return a random number 
+app.get('/random', (req, resp) => 
+{
+    resp.writeHead(200);
+    resp.end(`random number = ${Math.random() * 100}`)
+})
+
+app.get('/add', (req, resp) => {
+    console.log(req.url)
+    console.log(req.query)
+    if (isNaN(Number(req.query.a))) {
+        resp.writeHead(400);
+        resp.end(`${req.query.a} is not a number!`);
+        return;
+    }
+    if (isNaN(Number(req.query.b))) {
+        resp.writeHead(400);
+        resp.end(`${req.query.b} is not a number!`);
+        return;
+    }    
+    const a = Number(req.query.a)
+    const b = Number(req.query.b)
+    const sum = a + b
+    resp.writeHead(200);
+    resp.end(`${a} + ${b} = ${sum}`)
+})
 
 app.listen(port, () => console.log(`Listening to port ${port}`))
